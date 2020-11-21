f4ae739d9fbe22813f02b003d7fb66c7478503cc
MinchaeGwon <61741340+MinchaeGwon@users.noreply.github.com>
2020-11-22 04:08:00 +0900

    Update RegisterUserController.java

--- newmadamequery/src/main/java/controller/user/RegisterUserController.java
+++ newmadamequery/src/main/java/controller/user/RegisterUserController.java
@@ -30,13 +30,13 @@ public class RegisterUserController implements Controller {
 		try {
 			Manager manager = Manager.getInstance();
 			manager.createUser(user);
-		    return "redirect:/user/list";	// 성공 시 사용자 리스트 화면으로 redirect
+		    return "redirect:/user/mainPage";
 		        
-		} catch (ExistingException e) {	// 예외 발생 시 회원가입 form으로 forwarding
+		} catch (ExistingException e) {
 	        request.setAttribute("registerFailed", true);
 			request.setAttribute("exception", e);
 			request.setAttribute("user", user);
-			return "/user/registerForm.jsp";
+			return "/user/user_write.jsp";
 		}
 	}
 	
