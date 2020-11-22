bffa69597b7dce0209f1a52b1e75f6f1db363f77
MinchaeGwon <61741340+MinchaeGwon@users.noreply.github.com>
2020-11-23 05:44:10 +0900

    모든 게시글, 특정 게시글 조회 controller 추가

---
+++ newmadamequery/src/main/java/controller/post/ListPostController.java
@@ -0,0 +1,33 @@
+package controller.post;
+
+import java.util.ArrayList;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import controller.Controller;
+import model.Post;
+import model.service.Manager;
+import model.service.NotFoundException;
+
+public class ListPostController implements Controller {
+	@Override
+	public String execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		int groupId = Integer.parseInt(request.getParameter("groupId"));
+		
+		try {
+			Manager manager = Manager.getInstance();
+			ArrayList<Post> postList = manager.getAllPost(groupId);
+			
+			request.setAttribute("postList", postList);
+			
+			return "/mystudy/mystudy_view.jsp";
+		} catch (NotFoundException e) {
+	        request.setAttribute("Failed", true);
+			request.setAttribute("exception", e);
+			
+			return "/mystudy/addStudyboard.jsp"; //보여줄 스터디그룹 목록 없으면 스터디그룹 추가 페이지로 이동
+		}
+		
+	}
+}
