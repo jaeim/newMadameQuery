8b6eabbda19886dd66a9467245bd455797216d08
lhuna <lhuna@LAPTOP-2L1A51GK>
2020-11-24 23:04:21 +0900

    loginController 수정

--- newmadamequery/src/main/java/controller/user/LoginController.java
+++ newmadamequery/src/main/java/controller/user/LoginController.java
@@ -24,14 +24,15 @@ public class LoginController implements Controller {
 			User user = manager.findUser(email);
 			HttpSession session = request.getSession();
             session.setAttribute(UserSessionUtils.USER_SESSION_KEY, user.getMember_id());
-            
-            return "redirect:/user/list";			
+           
+            return "redirect:/user/mainPage.jsp";			
 		} catch (Exception e) {
+			
 			if(e instanceof NotFoundException) {request.setAttribute("notFound", true);}
 			if(e instanceof PasswordMismatchException) {request.setAttribute("passwordMismatch", true);}
             request.setAttribute("loginFailed", true);
 			request.setAttribute("exception", e);
-            return "/user/loginForm.jsp";			
+            return "/user/login.jsp";			
 		} 
     }
 }
