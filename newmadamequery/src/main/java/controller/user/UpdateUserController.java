474797be74e9ae328e227543bf2ed3290af494ed
MinchaeGwon <61741340+MinchaeGwon@users.noreply.github.com>
2020-11-27 07:00:06 +0900

    update user 수정중

--- newmadamequery/src/main/java/controller/user/UpdateUserController.java
+++ newmadamequery/src/main/java/controller/user/UpdateUserController.java
@@ -13,45 +13,46 @@ public class UpdateUserController implements Controller {
 	@Override
 	public String execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
 		if (request.getMethod().equals("GET")) {
+			System.out.println(1111);
 			// GET request: 회원정보 수정 form 요청
 
-			// 이메일을 파라미터로 받아야함
-			String email = request.getParameter("email");
-			// int updateId = Integer.parseInt(request.getParameter("userId"));
+			int updateId = Integer.parseInt(request.getParameter("userId"));
 
 			Manager manager = Manager.getInstance();
-			// memberId를 setAttribute로 보내야함
-			User user = (User) manager.findUser((int)(request.getAttribute("memberId")));
+			
+			User user = (User) manager.findUser(updateId);
 			request.setAttribute("user", user);
 
 			HttpSession session = request.getSession();
 			
 			int userId;
 			if(UserSessionUtils.hasLogined(session)) {
+				System.out.println(2222);
 				userId = UserSessionUtils.getLoginUserId(session);
 			}else {
 				return "redirect:/user/login";
 			}
 			
-			// 로그인할때 이메일 사용하니까 이메일을 매개변수로 보냄
 			if (UserSessionUtils.isLoginUser(userId, session)) {
+				System.out.println(3333);
 				return "/user/updateForm.jsp";
 			}
 
+			System.out.println(4444);
 			request.setAttribute("updateFailed", true);
 			request.setAttribute("exception", new IllegalStateException("타인의 정보는 수정할 수 없습니다."));
 			return "/user/myPage.jsp";
 		}
 
 		// POST request (회원정보가 parameter로 전송됨)
-		java.text.DateFormat df = new java.text.SimpleDateFormat("yyyy/MM/dd");
-		java.util.Date dob = df.parse(request.getParameter("birthday"));
+		System.out.println(5555);
+		//java.text.DateFormat df = new java.text.SimpleDateFormat("yyyy/MM/dd");
+		//java.util.Date dob = df.parse(request.getParameter("birthday"));
 
 		User updateUser = new User(
 				request.getParameter("email"), 
 				request.getParameter("password"),
 				request.getParameter("name"), 
-				dob, 
 				request.getParameter("phone"), 
 				request.getParameter("university"),
 				request.getParameter("department"), 
